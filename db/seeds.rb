af2c0fdabbf5fa01914aa40e8ea319f84d3ef58c
nader <alghazu@gmail.com>
2018-05-10 23:39:10 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  name: 'Nader',
+  email: 'nader@example.com',
+  password: '123456'
+)
+
+Recipe.create(
+  title: 'my best recipe',
+  descripton: 'italian dish',
+  ingredients: 'tomato, potato',
+  directions: 'put them tobether'
+)
